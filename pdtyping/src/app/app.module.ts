73afe6c644b7909d6f4a39346101114fd69eaadf
elvan <elvancanbasaran@gmail.com>
2022-03-10 14:07:54 +0300

    $process.env.npm_config_commitmsg

--- pdtyping/src/app/app.module.ts
+++ pdtyping/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
